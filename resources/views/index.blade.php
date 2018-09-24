c2562c9bf4cc39b9d9acdaa4f28693a775419267
Noah <noahtelussa@outlook.com>
2018-09-24 17:31:33 +0200

    adsf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,14 +64,14 @@
                         Mijn naam is Noah Telussa, {{$age}} jarige developer die in Januari 2017 afgestudeerd is als
                         Mediaontwikkelaar.
                         Deze opleiding heb ik afgerond in drie en een half jaar en heb hierdoor bij verschillende
-                        bedrijven stage gelopen, &#232;&#232;n
+                        bedrijven stage gelopen, &#233;&#233;n
                         van die bedrijven was het bedrijf Persc. Na mijn opleiding ben ik van Januari 2017 tot September
                         2017 full-time aan de slag gegaan bij
                         het bedijf Persc, hier heb ik aan verschillende websites gewerkt.
                     </p>
                     <hr>
                     <p>
-                        Van September 2017 tot heden ben ik begonnen aan de opleiding HBO-ICT in den haag, mijn doel van
+                        Van September 2017 tot heden ben ik begonnen aan de opleiding HBO-ICT in Den Haag, mijn doel van
                         deze opleiding is
                         mijn programmeer skills (vooral back-end) uit te breiden zodat ik niet alleen websites kan maken
                         maar ook programma's en apps.
